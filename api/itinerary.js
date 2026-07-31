797f2cb247c17e43d02142210da79ea494df97a8
fooxluigi-dev <foox.luigi@gmail.com>
2026-07-31 18:51:34 +0200

    cleanup: rimuovi debug

--- api/itinerary.js
+++ api/itinerary.js
@@ -184,10 +184,6 @@ REGOLE:
       days,
       tips,
       _failedDays: failedDays,
-      _debug: {
-        plannerRaw: plannerResp.slice(0, 300),
-        dayRaw: dayReplies[0]?.slice(0, 300) || '',
-      },
     });
 
   } catch (err) {
